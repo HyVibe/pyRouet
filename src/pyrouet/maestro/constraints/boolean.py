a06fccc4b2dea469925b27ca34d98a223bfd8cae
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-02-14 18:32:09 +0100

    Constraints as dataclasses

--- src/pyrouet/maestro/constraints/boolean.py
+++ src/pyrouet/maestro/constraints/boolean.py
@@ -9,23 +9,32 @@
 """
 
 from pyrouet.maestro.objects.constraints import Constraint_Object
+from dataclasses                         import dataclass, field
 
 
+@datalcass
 class Constraint_Boolean(Constraint_Object):
-    def __init__( self, ref_value ):
-        super().__init__("boolean")
-        self.ref_value = ref_value
+    name = "boolean"
+    ref_value: any
 
     def _validate(self,v):
         return v == self.ref_value
-    def __str__(self):
-        return f"{self.ref_value}"
-        
-    def data(self):
-        d = super().data()
-        d.update({
-            "ref_value" : bool(self.ref_value)
-        })
-        return d
 
-
+#class Constraint_Boolean(Constraint_Object):
+#    def __init__( self, ref_value ):
+#        super().__init__("boolean")
+#        self.ref_value = ref_value
+#
+#    def _validate(self,v):
+#        return v == self.ref_value
+#    def __str__(self):
+#        return f"{self.ref_value}"
+#        
+#    def data(self):
+#        d = super().data()
+#        d.update({
+#            "ref_value" : bool(self.ref_value)
+#        })
+#        return d
+#
+#
--- src/pyrouet/maestro/objects/constraints.py
+++ src/pyrouet/maestro/objects/constraints.py
@@ -8,24 +8,35 @@
  Revised February 2022
 """
 
-from abc import ABC,abstractmethod
+from abc         import ABC,abstractmethod
+from dataclasses import dataclass, field
 
 
 # ┌────────────────────────────────────────┐
 # │ Default constraints                    │
 # └────────────────────────────────────────┘
+@dataclass
 class Constraint_Object:
-    def __init__( self, __class ):
-        self.__class = __class
-    
+    name: str = field(init=None)
+
     def validate(self,v):
-        if v is None : return False
-        else         : return bool(self._validate(v))
+        if v is None: return False
+        else        : return bool(self._validate(v))
 
     @abstractmethod
-    def _validate(self,v):pass
+    def _validate(self,v): pass
 
-    def data(self): return {
-        "class": self.__class
-    }
 
+if __name__ == "__main__":
+    from pprint      import pprint
+    from dataclasses import asdict
+
+    @dataclass
+    class Constraint_Dumb(Constraint_Object):
+        name = "test"
+
+    def _validate(self,v):
+        return True
+
+    xx = Constraint_Dumb()
+    print(asdict(xx))
