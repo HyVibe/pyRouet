53cf8033da6486e8f4abc6e586014b1564611d73
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2022-03-20 00:12:35 +0100

    Show debug info when running tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ test_to_run="$1"
 [ -z $test_to_run ] && test_to_run="tests"
 
 export PYTHONPATH=${PYTHONPATH}:"$(pwd)/src/" &&
-./.env/bin/python -m pytest --cov=src --cov-report html --cov-report term-missing "$test_to_run"
+./.env/bin/python -m pytest --cov=src --cov-report html --cov-report term-missing -o log_cli_level=DEBUG "$test_to_run"
